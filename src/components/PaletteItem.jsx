33abcdf991f9263573d1034b059fb8e2d890ff7d
Bence <borsi92@gmail.com>
2021-01-14 18:01:09 +0100

    added design to th details section

--- src/components/PaletteItem.jsx
+++ src/components/PaletteItem.jsx
@@ -1,10 +1,27 @@
 import React, { useEffect, useState, useContext } from 'react';
+import './PaletteItem.css'
 
 const PaletteItem = (props) => {
 
+	const getDate = (stringTime) => {
+		let fullDate = stringTime;
+		fullDate = fullDate.split(' ');
+		let time = fullDate[1];
+		return time;
+	}
+
 	return (
 		<div className='palette'>
-			<h2>{props.palette.title}</h2>
+			<div className='paletteDetails'>
+				<div className='paletteTitle'>{props.palette.title}</div>
+				<div className='paletteNameAndDate'>by {props.palette.userName} at {getDate(props.palette.dateCreated)}</div>
+				<div className='votesAndViews'>{props.palette.numViews} views {props.palette.numVotes} votes</div>
+			</div>
+
+			<div>
+				
+			</div>
+			
 		</div>
 	);
 };
