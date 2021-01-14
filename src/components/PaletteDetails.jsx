2c4f1c788965c1caaf241b60a9d8481da748acb2
Bence <borsi92@gmail.com>
2021-01-14 13:48:56 +0100

    cleaned up original code and added PaletteDetails component

---
+++ src/components/PaletteDetails.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState, useContext } from 'react';
+
+const PaletteDetails = (props) => {
+
+	return (
+		<div className='wholePage' style={{ textAlign: 'center' }}>
+			<h1>Hii</h1>
+		</div>
+	);
+};
+
+export default PaletteDetails;
